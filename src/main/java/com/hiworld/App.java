f927e36b76e711d5bfc58635378510de9d5d84ad
lvsp <shuai1107@163.com>
2021-02-26 11:43:37 +0800

    test

---
+++ src/main/java/com/hiworld/App.java
@@ -0,0 +1,13 @@
+package com.hiworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
